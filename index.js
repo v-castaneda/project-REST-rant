da2bdb8bfcc1d305531f38133649dabce27c022e
v-castaneda <103009800+v-castaneda@users.noreply.github.com>
2022-08-10 10:26:52 -0400

    Adding 404 page route

--- index.js
+++ index.js
@@ -7,5 +7,12 @@ const express = require("express");
 // initialize the app object
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("*", (req, res) => {
+  res.status(404).send("<h1>404 Page</h1>");
+});
+
 app.listen(process.env.PORT, () => console.log("I am very awake!"));
