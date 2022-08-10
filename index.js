5801d58b18f8a893b20e57bd21f22649c0e85f23
v-castaneda <103009800+v-castaneda@users.noreply.github.com>
2022-08-10 10:42:40 -0400

    updating index.js with controller

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const express = require("express");
 // initialize the app object
 const app = express();
 
+// import router
+app.use("/places", require("./controllers/places"));
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
@@ -15,4 +18,4 @@ app.get("*", (req, res) => {
   res.status(404).send("<h1>404 Page</h1>");
 });
 
-app.listen(process.env.PORT, () => console.log("I am very awake!"));
+app.listen(process.env.PORT, () => console.log("I am awake!"));
