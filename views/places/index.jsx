a38ea16cda2a9c35635a63239212c68d2df49099
v-castaneda <103009800+v-castaneda@users.noreply.github.com>
2022-08-13 01:17:33 -0400

    Adding places index page

---
+++ views/places/index.jsx
@@ -0,0 +1,14 @@
+const React = require("react");
+const Def = require("../default");
+
+function index() {
+  return (
+    <Def>
+      <main>
+        <h1>PLACES INDEX</h1>
+      </main>
+    </Def>
+  );
+}
+
+module.exports = index;
