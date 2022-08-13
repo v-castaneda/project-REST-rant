9a37629271a2388cf38a380e91471f41b2db0477
v-castaneda <103009800+v-castaneda@users.noreply.github.com>
2022-08-13 00:55:28 -0400

    Adding 404 page view

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("*", (req, res) => {
-  res.status(404).send("<h1>404 Page</h1>");
+  res.render("error404");
 });
 
 app.listen(process.env.PORT, () => console.log("I am awake!"));
