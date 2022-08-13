f8aa1bc001d3bb8ba4fa02a3dfa6a0f28f177553
v-castaneda <103009800+v-castaneda@users.noreply.github.com>
2022-08-13 11:01:32 -0400

    adding CSS bootstrap framework

--- views/default.jsx
+++ views/default.jsx
@@ -5,6 +5,10 @@ function Def(html) {
     <html>
       <head>
         <title>Title</title>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css"
+        />
       </head>
       <body>{html.children}</body>
     </html>
