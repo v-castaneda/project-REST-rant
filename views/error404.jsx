2759b209d86e5abacecb99c54aa7976d95927629
v-castaneda <103009800+v-castaneda@users.noreply.github.com>
2022-08-13 11:45:28 -0400

    adding image to error page

--- views/error404.jsx
+++ views/error404.jsx
@@ -7,6 +7,17 @@ function error404() {
       <main>
         <h1>404: PAGE NOT FOUND</h1>
         <p>Oops, sorry, we can't find this page!</p>
+        <div>
+          <img
+            src="/images/chia-fruit-drink.jpg"
+            alt="Chia Fruit Shake"
+            width="40%"
+          />
+          <div>
+            Photo by <a href="AUTHOR_LINK">Brenda Godinez</a> on{" "}
+            <a href="UNSPLASH_LINK">Unsplash</a>
+          </div>
+        </div>
       </main>
     </Def>
   );
