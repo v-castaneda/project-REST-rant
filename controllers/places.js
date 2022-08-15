d339fdcec01ce2528870047b035bcddbffc73ccb
v-castaneda <103009800+v-castaneda@users.noreply.github.com>
2022-08-14 23:27:14 -0400

    New places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,23 +1,8 @@
 const router = require("express").Router();
+const places = require("../models/places.js");
 
 // GET /places
 router.get("/", (req, res) => {
-  let places = [
-    {
-      name: "H-Thai-ML",
-      city: "Seattle",
-      state: "WA",
-      cuisines: "Thai, Pan-Asian",
-      pic: "/images/restaurant-1.jpg",
-    },
-    {
-      name: "Coding Cat Cafe",
-      city: "Phoenix",
-      state: "AZ",
-      cuisines: "Coffee, Bakery",
-      pic: "/images/restaurant-2.jpg",
-    },
-  ];
   res.render("places/index", { places });
 });
 
@@ -29,7 +14,18 @@ router.get("/new", (req, res) => {
 // CREATE
 router.post("/", (req, res) => {
   console.log(req.body);
-  res.send("POST /places");
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = "http://placekitten.com/400/400";
+  }
+  if (!req.body.city) {
+    req.body.city = "Anytown";
+  }
+  if (!req.body.state) {
+    req.body.state = "USA";
+  }
+  places.push(req.body);
+  res.redirect("/places");
 });
 
 module.exports = router;
---
+++ models/places.js
@@ -0,0 +1,16 @@
+module.exports = [
+  {
+    name: "H-Thai-ML",
+    city: "Seattle",
+    state: "WA",
+    cuisines: "Thai, Pan-Asian",
+    pic: "/images/restaurant-1.jpg",
+  },
+  {
+    name: "Coding Cat Cafe",
+    city: "Phoenix",
+    state: "AZ",
+    cuisines: "Coffee, Bakery",
+    pic: "/images/restaurant-2.jpg",
+  },
+];
