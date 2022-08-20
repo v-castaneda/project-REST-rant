a81c713d880fe8db21abf91bc1bf703d5db2fa88
v-castaneda <103009800+v-castaneda@users.noreply.github.com>
2022-08-20 13:01:27 -0400

    Adding mongoose

--- models/places.js
+++ models/places.js
@@ -1,16 +1,29 @@
-module.exports = [
-  {
-    name: "H-Thai-ML",
-    city: "Seattle",
-    state: "WA",
-    cuisines: "Thai, Pan-Asian",
-    pic: "/images/restaurant-1.jpg",
-  },
-  {
-    name: "Coding Cat Cafe",
-    city: "Phoenix",
-    state: "AZ",
-    cuisines: "Coffee, Bakery",
-    pic: "/images/restaurant-2.jpg",
-  },
-];
+const mongoose = require('mongoose')
+
+const placeSchema = new mongoose.Schema({
+  name: { type: String, required: true},  
+  pic: String, 
+  cuisines: { type: String, required: true},  
+  city: { type: String, default: 'Anytown'}, 
+  state: { type: String, default: 'USA'}, 
+  founded: Number
+})
+
+module.exports = mongoose.model('Place', placeSchema)
+
+// module.exports = [
+//   {
+//     name: "H-Thai-ML",
+//     city: "Seattle",
+//     state: "WA",
+//     cuisines: "Thai, Pan-Asian",
+//     pic: "/images/restaurant-1.jpg",
+//   },
+//   {
+//     name: "Coding Cat Cafe",
+//     city: "Phoenix",
+//     state: "AZ",
+//     cuisines: "Coffee, Bakery",
+//     pic: "/images/restaurant-2.jpg",
+//   },
+// ];
