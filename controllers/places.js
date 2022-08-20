9606e45a7b26c85f8bf70cdbc5890c3514b82c6a
v-castaneda <103009800+v-castaneda@users.noreply.github.com>
2022-08-20 13:08:22 -0400

    DELETING ORIGINAL ROUTERS PRIOR TO INTEGRATING MONGOOSE

--- controllers/places.js
+++ controllers/places.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const places = require("../models/places.js");
-
+// COMMIT PRIOR TO RETYPING ROUTERS TO CONNECT MONGOOSE
 // GET /places
 router.get("/", (req, res) => {
   res.render("places/index", { places });
