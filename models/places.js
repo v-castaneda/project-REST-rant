6739b0c01ba2cf50dd94bcf0cf2d6b53bc5f5b9a
v-castaneda <103009800+v-castaneda@users.noreply.github.com>
2022-08-20 14:10:11 -0400

    fixed default image

--- models/places.js
+++ models/places.js
@@ -1,9 +1,8 @@
 const mongoose = require('mongoose')
-let url = 'https://placekitten.com/300/300'
 
 const placeSchema = new mongoose.Schema({
   name: { type: String, required: true},  
-  pic: {type: String, default: url}, 
+  pic: String, 
   cuisines: { type: String, required: true},  
   city: { type: String, default: 'Anytown'}, 
   state: { type: String, default: 'USA'}, 
