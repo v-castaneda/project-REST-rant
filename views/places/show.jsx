75eda53ba438689a837f9dd60fb50533ea38a861
v-castaneda <103009800+v-castaneda@users.noreply.github.com>
2022-08-22 21:11:14 -0400

    Created helper method

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -5,24 +5,43 @@ function show(data) {
   return (
     <Def>
       <main>
-        <h1>{data.place.name}</h1>
-        <h2>Rating</h2>
-        <h3>Not Rated</h3>
-        <h2>Description</h2>
-        <h3>
-          Located in {data.place.city}, {data.place.state} and serving{" "}
-          {data.place.cuisines}
-        </h3>
-        <h2>Comments</h2>
-        <h3>No Comments yet!</h3>
-        <a href={`/places/${data.id}/edit`} className="btn btn-warning">
-          Edit
-        </a>
-        <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
-          <button type="submit" className="btn btn-danger">
-            Delete
-          </button>
-        </form>
+        <div className="row">
+          <div className="col-sm-6">
+            <img src={data.place.pic} alt={data.place.name}  width="100%"/>
+            <h3>
+              Located in {data.place.city}, {data.place.state}
+            </h3>
+          </div>
+
+          <div className="col-sm-6">
+            <h1>{data.place.name}</h1>
+            <h2>Rating</h2>
+            <h3>Not Rated</h3>
+            <h2>Description</h2>
+            <h3>
+              {data.place.showEstablished()}
+            </h3>
+            <h4>
+              Serving {data.place.cuisines}
+            </h4>
+            
+            <a href={`/places/${data.id}/edit`} className="btn btn-warning">
+            Edit
+            </a>
+
+            <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
+              <button type="submit" className="btn btn-danger">
+                Delete
+              </button>
+            </form>
+
+          </div>
+
+          <h2>Comments</h2>
+          
+          <h3>No Comments yet!</h3>
+
+        </div>
       </main>
     </Def>
   );
