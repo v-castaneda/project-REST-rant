dddd4931a36f11fb31eb286e8a8916b2daa22066
v-castaneda <103009800+v-castaneda@users.noreply.github.com>
2022-08-24 21:05:12 -0400

    fixing default image

--- models/places.js
+++ models/places.js
@@ -1,16 +1,14 @@
 const mongoose = require('mongoose')
 
 const placeSchema = new mongoose.Schema({
-  name: { type: String, required: true},  
-  pic: {
-    type: String
-  }, 
-  cuisines: { type: String, required: true},  
-  city: { type: String, default: 'Anytown'}, 
-  state: { type: String, default: 'USA'}, 
+  name: { type: String, required: true },
+  pic: { type: String},
+  cuisines: { type: String, required: true },
+  city: { type: String, default: 'Anytown' },
+  state: { type: String, default: 'USA' },
   founded: {
-    type: Number, 
-    min: [1673, 'Surely not that old?'], 
+    type: Number,
+    min: [1673, 'Surely not that old?!'],
     max: [new Date().getFullYear(), 'Hey, this year is in the future!']
   }
 })
