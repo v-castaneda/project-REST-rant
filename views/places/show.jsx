822f960caf38898090699f2e6fc54c563d283bfa
v-castaneda <103009800+v-castaneda@users.noreply.github.com>
2022-08-25 08:21:51 -0400

    populated the comments section

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,6 +2,27 @@ const React = require("react");
 const Def = require("./default");
 
 function show(data) {
+  let comments = (
+    <h3 className="inactive">
+      No comments yet!
+    </h3>
+  )
+
+  if (data.place.comments.length) {
+    comments = data.place.comments.map(c => {
+      return (
+        <div className="border">
+          <h2 className="rant">{c.rant ? 'Rant! 😒': 'Rave! 😻 🥳'}</h2>
+          <h4>{c.content}</h4>
+          <h3>
+            <stong>- {c.author}</stong>
+          </h3>
+          <h4>Rating: {c.stars}</h4>
+        </div>
+      )
+    })
+  }
+
   return (
     <Def>
       <main>
@@ -38,6 +59,7 @@ function show(data) {
           </div>
 
           <h2>Comments</h2>
+          {comments}
           
           <h3>No Comments yet!</h3>
 
